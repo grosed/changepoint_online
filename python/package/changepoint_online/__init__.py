c503e0618a214bacd1d9d2bae75c22f48c5f235e
gtromano <g.romano@lancaster.ac.uk>
2024-04-25 12:04:24 +0100

    removed title

--- python/package/changepoint_online/__init__.py
+++ python/package/changepoint_online/__init__.py
@@ -70,6 +70,5 @@ along with this program. If not, see <https://www.gnu.org/licenses/gpl-3.0.txt>.
 
 from .focus import Focus, Gaussian, Bernoulli, Poisson, Gamma, Exponential, NPFocus
 
-__title__ = 'changepoint_online'
 __version__ = '0.0.3'
 __author__ = 'Gaetano Romano, Daniel Grose'
